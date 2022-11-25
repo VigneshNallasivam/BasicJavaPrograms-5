9dda0415d20c0457e751f16e2d33349865955c35
VigneshNallasivam <vickyshanestark@gmail.com>
2022-11-25 12:59:24 +0530

    Added : To Check whether the Given letter is Vowel or Consonant

---
+++ src/com/Java/basics/ConsonantVowel.java
@@ -0,0 +1,24 @@
+package com.Java.basics;
+
+public class ConsonantVowel 
+{
+	public static void main(String args[]) 
+    {
+
+        char letter = 'v';
+        switch (letter) 
+        {
+            case 'a':System.out.println(letter + " is vowel");
+                     break;
+            case 'e':System.out.println(letter + " is vowel");
+                     break;
+            case 'i':System.out.println(letter + " is vowel");
+                     break;
+            case 'o':System.out.println(letter + " is vowel");
+                     break;
+            case 'u':System.out.println(letter + " is vowel");
+                     break;
+            default:System.out.println(letter + " is consonant");
+        }
+    }
+}
